391804a7735bdf27814064b0259f501764e5d5b2
[Orelie] <[orelie.canu@gmail.com]>
2021-06-29 15:54:50 +0200

    ajout de commentaires pour GROUPE

---
+++ nouvelle_branche.js
@@ -0,0 +1,10 @@
+// On en a déjà utilisé !
+console.log("bonjour");
+
+// Qu'est-ce qu'une chaîne de caractères ? Un caractère ?
+// @&é"'(§è!çà)-azertyuiop^$qsdfghjklmù`<wxcvbn,;:=#1234567890°_AZERTYUIOP¨*QSDFGHJKLM%£>WXCVBN?./+
+
+// Emojis
+console.log("🔥😁");
+
+//j'ajoute des commentaire pour mon groupe
